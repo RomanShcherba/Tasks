a82715159e4afd22a6fbe061442b7962e754d2d5
Roman Shcherba <warboyrom@gmail.com>
2024-08-01 13:44:10 +0300

    some changes

--- Tasks/ValidHexCode.cs
+++ Tasks/ValidHexCode.cs
@@ -14,18 +14,18 @@ namespace Tasks
         /// <summary>
         /// Validation of hex code
         /// </summary>
-        /// <param name="inputHex)">
+        /// <param name="inputHex">
         /// input hex code
         /// </param>
         /// <returns></returns>
         public static bool CheckIsHexValid(string inputHex)
         {
-            if (inputHex.Length != 8 && !inputHex.StartsWith("0x"))
+            if (inputHex.Length == 8 && inputHex.StartsWith("0x"))
             {
-                return false;
+                return inputHex.Substring(2).All(x => "0123456789ABCDEFabcdef".Contains(x));
             }
-         
-            return inputHex.Substring(2).All(x => "0123456789ABCDEFabcdef".Contains(x));
+
+            return false;
         }
     }
 }
