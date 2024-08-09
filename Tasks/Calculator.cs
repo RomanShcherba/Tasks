4444bb5252c851c56f5b7da28f3dbf354ab4a678
Roman Shcherba <warboyrom@gmail.com>
2024-08-09 16:27:26 +0300

    Calculator

---
+++ Tasks/Calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    /// <summary>
+    /// Calculator
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Calculate operations
+        /// </summary>
+        /// <param name="a">
+        /// First number
+        /// </param>
+        /// <param name="b">
+        /// Second number
+        /// </param>
+        /// <param name="actions">
+        /// Math operators
+        /// </param>
+        /// <exception cref="DivideByZeroException">
+        /// Divide by zero exception
+        /// </exception>
+        /// <exception cref="Exception">
+        /// Exception of zero numbers of roots
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Argument out of range exception
+        /// </exception>
+        public static double Calculate(double a, double b, string actions)
+        {
+            switch (actions)
+            {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    if (b == 0)
+                    {
+                        throw new DivideByZeroException("You can't divide by zero");
+                    }
+                    return a / b;
+                case "^":
+                    return Math.Pow(a, b);
+                case "V":
+                    if (a > 0 || b > 0)
+                    {
+                        return Math.Pow(a, 1.0 / b);
+                    }
+                    throw new Exception("invalid operation");
+                default:
+                throw new ArgumentOutOfRangeException(nameof(actions), "Out of range");
+
+            }
+        }
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    /// <summary>
+    /// Program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main
+        /// </summary>
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter number:");
+
+                double a = Convert.ToDouble(Console.ReadLine());
+                string action = Console.ReadLine();
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                double result = Calculator.Calculate(a, b, action);
+                Console.WriteLine(result);
+            }
+            catch(ArgumentOutOfRangeException ex) 
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"{ex.Message}\n");
+            }
+            catch (Exception ex)  
+            {
+                Console.WriteLine($"{ex.Message}\n");    
+            }
+        }
+    }
+}
