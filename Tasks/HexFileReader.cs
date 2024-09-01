669b90a5d1c21303b0438f7be23af668339a84e1
Roman Shcherba <warboyrom@gmail.com>
2024-09-01 13:16:08 +0300

    Added classes

---
+++ Tasks/HexFileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class HexFileReader
+    {
+        public byte[] ReadHexString(string filePath)
+        {
+            string hexString = File.ReadAllText(filePath);
+        }
+    }
+}
