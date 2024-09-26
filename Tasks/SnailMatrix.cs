c802a97396cfc76a4a3751c6e86c55b31680687c
Roman Shcherba <warboyrom@gmail.com>
2024-09-26 14:40:16 +0300

    Matrix

---
+++ Tasks/SnailMatrix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class SnailArray
+    {
+        public static void MatrixNumberRange (int rows, int columns)
+        {
+            if (rows < 0 || rows > 101)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rows));
+            }
+            if (columns < 0 || columns > 101)
+            {
+                throw new ArgumentOutOfRangeException(nameof(columns));
+            }
+        }
+        public static void Matrix(int[,] matrix, Random random)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < columns; y++)
+                {
+                    matrix[x, y] = random.Next(0, 101);
+                }
+            }
+        }
+        public static int ValidInput()
+        {
+            int input;
+
+            while (int.TryParse(Console.ReadLine(), out input) || input <= 0 || input >= 46340)
+            {
+                 Console.WriteLine("Invalid input");
+            }
+            return input;
+        }
+
+        public static void PrintDiagonal(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            
+            for(int x = 0;x < rows; x++)
+            {
+                for(int y = 0;y < columns; y++)
+                {
+
+                }
+            }
+        }
+    }
+}
